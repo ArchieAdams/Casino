5995338fc4c59a38b56d2a2ce48ee84b7dfdaae7
Archie <archieadams15@gmail.com>
2021-01-21 20:55:59 +0000

    Basic Player
    Basic player with movment but no animations

---
+++ Casino/Assets/Script/Jump.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    [SerializeField]
+    GroundCheck groundCheck;
+    Rigidbody _rigidBody;
+    private Animator _animator;
+    public float jumpStrength = 2;
+    public event System.Action Jumped;
+
+
+    void Reset()
+    {
+        groundCheck = GetComponentInChildren<GroundCheck>();
+        if (!groundCheck)
+            groundCheck = GroundCheck.Create(transform);
+    }
+
+    void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void LateUpdate()
+    {
+        if (Input.GetButtonDown("Jump") && groundCheck.isGrounded)
+        {
+            _rigidBody.AddForce(Vector3.up * 100 * jumpStrength);
+            Jumped?.Invoke();
+            _animator.SetBool("Jump", true);
+        }
+        else
+        {
+            _animator.SetBool("Jump", false);
+        }
+    }
+}
